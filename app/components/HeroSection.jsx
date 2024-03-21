73ec1e120f3cc1596ea64eae9245b893b2b2e4d6
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-03-21 17:57:36 +0100

    added Hero/header section

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hi, i'm Matias</h1>
+        <p className="text-[#ADB7BE] py-10 text-lg lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quod minus nesciunt odio quisquam a ea, autem aliquam perferendis similique? Laborum ducimus ipsa dolorum labore maiores rerum iste facere iure!
+        </p>
+        
+    </section>
+      
+  );
+};
+
+export default HeroSection;
