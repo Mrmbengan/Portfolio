3f6ab5b99ee5e5868d127c027f2572ee7d946f98
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-04-03 17:28:31 +0200

    projects section done

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import ProjectCard from './ProjectCard'
+import ProjectTag from './ProjectTag'
 
 const projectData = [
   {
     id:1,
     title:"React Portfolio Website",
     description:"A portfolio website built with React and Tailwind CSS",
-    image:"/images/projects/React-Portfolio.png",
+    image:"/images/projects/ReactPortfolio.png",
     link:"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    tag: ["React", "Next.js", "Tailwind"],
+    tag: ["All", "Web", "Mobile", "React", "Next.js", "Tailwind"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -18,7 +20,7 @@ const projectData = [
     description:"A Webshop built with React",
     image:"/images/projects/React-webshop.png",
     link:"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    tag: ["React",],
+    tag: ["All", "Web", "Mobile", "React",],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -28,7 +30,7 @@ const projectData = [
     description:"A Landing Page built Tailwind CSS",
     image:"/images/projects/loopstudioDeskMobile.png",
     link:"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    tag: [ "Tailwind"],
+    tag: ["All","Web", "Mobile", "Tailwind"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -38,17 +40,17 @@ const projectData = [
     description:"Alanding page built Tailwind CSS",
     image:"/images/projects/tailwind-landing.png",
     link:"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    tag: ["React", "Next.js", "Tailwind"],
+    tag: ["All","Web", "Mobile", "React", "Next.js", "Tailwind"],
     gitUrl: "/",
     previewUrl: "/",
   },
   {
     id:5,
     title:"Rock, Papers scissors",
-    description:"rock, papers scissrs game",
+    description:"rock, papers scissors game",
     image:"/images/projects/rock-paper-sissors.png",
     link:"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    tag: [ "Js"],
+    tag: ["All", "Web", "Js"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -58,17 +60,43 @@ const projectData = [
     description:"webshop built with Wordpress and PHP",
     image:"/images/projects/Worpress.png",
     link:"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    tag: ["Wordpress", "PHP",],
+    tag: ["All", "Wordpress", "PHP",],
     gitUrl: "/",
     previewUrl: "/",
   }
 ]
 const ProjectsSection =() => {
+  const [tag, setTag] = useState("All")
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag)
+  };
+
+  const filteredProjects = projectData.filter((project) =>
+    project.tag.includes(tag)
+  );
   return (
     <>
       <h2 className='text-center text-4xl font-bold text-white mt-4 mb-4'>My Projects</h2>
+      <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+        <ProjectTag 
+          onClick={handleTagChange} 
+          name="All" 
+          isSelected={tag === "All"} 
+        />
+        <ProjectTag 
+          onClick={handleTagChange} 
+          name="Web" 
+          isSelected={tag === "Web"} 
+        />
+        <ProjectTag 
+          onClick={handleTagChange} 
+          name="Mobile" 
+          isSelected={tag === "Mobile"} 
+        />
+      </div>
       <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-        {projectData.map((project) => (
+        {filteredProjects.map((project) => (
         <ProjectCard key={project.id} 
         title={project.title} 
         description={project.description} 
