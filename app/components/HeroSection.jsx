7fd019ff9d2ddcfbf073f14c1d734a5237d48b3b
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-04-04 22:35:35 +0200

    bugg fix heroSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-white">
-                Hello, i'm {" "} 
+                Hello, i&apos;m {" "} 
             </span>
             <br />
             <TypeAnimation
